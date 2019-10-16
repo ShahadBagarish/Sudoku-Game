2910a895c832f15e99fd6387a84edbbe74697a5b
Shahad A. Bagarish <shahad.bagarish@gmail.com>
2019-10-16 10:22:08 +0300

    version 5

--- assets/main.js
+++ assets/main.js
@@ -1,43 +1,108 @@
-
 // Declaration Space ------------------
 let number_holder = ''
 let place_holder = ''
 let bool = true
-let correctPlacesForOne = ["17", "32", "48", "53", "73", "97"]
-let correctPlacesForTwo = ["16", "29", "33", "45", "62", "74", "87"]
-let correctPlacesForThree = ["27", "34", "49", "77", "82", "95"]
-let correctPlacesForFour = ["13", "28", "35", "59", "61", "78", "93"]
-let correctPlacesForFive = ["37", "42", "55", "68", "79", "81"]
-let correctPlacesForSix = ["21", "46", "89", "94"]
-let correctPlacesForSeven = ["15", "39", "51", "64", "76", "83", "98"]
-let correctPlacesForEight = ["23", "36", "67", "75",]
-let correctPlacesForNine = ["31", "43", "57", "65", "71"]
-let place = ''
-let Minutes = 0;
-let seconds = 0;
-let timerId = 0;
-let hasStarted = false;
+let id_out, id_in = ''
+let x, y = 0
+
+let board_hard = [
+
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 0, 9]
+]
+
+let solved_board_hard = [
+    [5, 3, 4, 6, 7, 8, 1, 9, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 9, 7, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 7, 1, 9]
+]
+
+let board_medium = [
+    [0, 0, 1, 0, 0, 0, 0, 0, 4],
+    [0, 9, 0, 0, 2, 0, 0, 5, 3],
+    [0, 8, 0, 0, 0, 0, 7, 0, 0],
+    [0, 1, 0, 4, 3, 0, 0, 0, 0],
+    [0, 0, 7, 0, 0, 0, 0, 0, 9],
+    [0, 0, 0, 0, 0, 0, 0, 0, 2],
+    [0, 6, 8, 1, 0, 0, 0, 4, 0],
+    [0, 0, 0, 9, 7, 0, 8, 0, 0],
+    [0, 0, 2, 0, 0, 0, 0, 0, 0],
+]
+
+let solved_board_medium = [
+    [3, 2, 1, 7, 6, 5, 9, 8, 4],
+    [7, 9, 4, 8, 2, 1, 6, 5, 3],
+    [6, 8, 5, 3, 4, 9, 7, 2, 1],
+    [2, 1, 6, 4, 3, 8, 5, 7, 9],
+    [8, 4, 7, 5, 9, 6, 1, 3, 2],
+    [5, 3, 9, 2, 1, 7, 4, 6, 8],
+    [9, 6, 8, 1, 5, 3, 2, 4, 7],
+    [4, 5, 3, 9, 7, 2, 8, 1, 6],
+    [1, 7, 2, 6, 8, 4, 3, 9, 5],
+]
+let board_easy = [
+
+    [0, 7, 3, 5, 9, 0, 1, 6],
+    [0, 0, 4, 1, 2, 0, 0, 0, 0],
+    [1, 0, 3, 7, 4, 8, 0, 0, 5],
+    [0, 1, 5, 8, 0, 0, 6, 7, 2],
+    [2, 4, 6, 5, 1, 7, 3, 9, 8],
+    [0, 7, 8, 9, 6, 2, 0, 5, 0],
+    [4, 0, 9, 0, 8, 0, 0, 3, 7],
+    [6, 3, 0, 2, 7, 5, 8, 4, 9],
+    [7, 0, 2, 0, 9, 3, 0, 6, 0]
+]
+let solved_board_easy = [
+    [8, 2, 7, 3, 5, 9, 4, 1, 6],
+    [5, 9, 4, 1, 2, 6, 7, 8, 3],
+    [1, 6, 3, 7, 4, 8, 9, 2, 5],
+    [9, 1, 5, 8, 3, 4, 6, 7, 2],
+    [2, 4, 6, 5, 1, 7, 3, 9, 8],
+    [3, 7, 8, 9, 6, 2, 1, 5, 4],
+    [4, 5, 9, 6, 8, 1, 2, 3, 7],
+    [6, 3, 1, 2, 7, 5, 8, 4, 9],
+    [7, 8, 2, 4, 9, 3, 5, 6, 1]
+]
+
 
 // Click button --------------------
+
 $(".column1").hide();
 
 $("#home").click(function () {
+
     $(".HomePage_container").show();
     $(".number_container").hide();
     $(".game_container").hide();
     $("#game_logo").hide();
     $(".column1").hide();
+
+    fill_level(fill_zeros())
 });
 
 $("#save").click(function () {
 
 });
 
+$(".close").click(function () {
+    $(".modal").hide();
+});
+
 $("#play").click(function () {
     $(".modal").show();
-});
-$(".modal-content").click(function () {
-    $(".modal").hide();
 });
 
 $("#hard").click(function () {
@@ -50,45 +115,74 @@ $("#hard").click(function () {
     $(".column1").show();
 
     timer.setupListeners();
+    fill_level(board_hard)
+    play(solved_board_hard)
+});
+$("#medium").click(function () {
+    $(".modal").hide();
+    $(".HomePage_container").hide();
+
+    $(".number_container").show();
+    $(".game_container").show();
+    $("#game_logo").show();
+    $(".column1").show();
+
+    timer.setupListeners();
+    fill_level(board_medium)
+    play(solved_board_medium)
 });
 
+$("#easy").click(function () {
+    $(".modal").hide();
+    $(".HomePage_container").hide();
+
+    $(".number_container").show();
+    $(".game_container").show();
+    $("#game_logo").show();
+    $(".column1").show();
+
+    timer.setupListeners();
+    fill_level(board_easy)
+    play(solved_board_easy)
+});
 
 $(".number_container").click(function (e) {
     number_holder = e.target.id
     console.log(number_holder);
 });
 
-$(".game_container").click(function (e) {
-    place_holder = e.target
-    console.log(place_holder.id);
-    if (number_holder == '') {
-        Swal.fire({
-            type: 'info',
-            title: "Choose number from bar",
-            showConfirmButton: false,
-            timer: 1500
-        });
-    }
-    else if (returnCheck()) {
-        console.log(e.target);
-        $(e.target).css("background-color", "rgb(230, 185, 67)");
-        Swal.fire({
-            type: 'success',
-            title: "Correct Place",
-            showConfirmButton: false,
-            timer: 1500
-        });
-        $(`#${place}`).text(number_holder);
-        bool = false
-        number_holder = ''
-    } else { 
-        if (e.target.innerHTML) {
+// function ----------------------
+
+function play(solved_board) {
+    $(".game_container").click(function (e) {
+        place_holder = e.target.id
+        console.log(place_holder.id);
+        if (number_holder == '') {
+            Swal.fire({
+                type: 'info',
+                title: "Choose number from bar",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+        else if (e.target.innerHTML) {
             Swal.fire({
                 type: 'warning',
                 title: "The Place Equipped",
                 showConfirmButton: false,
                 timer: 1500
             });
+        } else if (returnCheck(solved_board)) {
+            $(e.target).css("background-color", "rgb(230, 185, 67)");
+            Swal.fire({
+                type: 'success',
+                title: "Correct Place",
+                showConfirmButton: false,
+                timer: 1500
+            });
+            $(`#${place_holder}`).text(number_holder);
+            bool = false
+            number_holder = ''
 
         } else {
             Swal.fire({
@@ -97,82 +191,52 @@ $(".game_container").click(function (e) {
                 showConfirmButton: false,
                 timer: 1500
             });
-        }
-    }
-});
 
-// function Space -----------------------
-function returnCheck() {
-    if (number_holder == "1") {
-        for (let i = 0; i < correctPlacesForOne.length; i++) {
-            if (place_holder.id == correctPlacesForOne[i]) {
-                place = correctPlacesForOne[i]
-                return true
+        }
+    });
+}
+
+function fill_level(board) {
+    for (var i = 0; i < board.length; i++) {
+        id_out = i + ''
+        for (let j = 0; j < board[i].length; j++) {
+            id_in = id_out + j + ''
+            if (board[i][j] != 0) {
+                $(`#${id_in}`).text(board[i][j]);
             }
         }
-    } else if (number_holder == "2") {
-        for (let i = 0; i < correctPlacesForTwo.length; i++) {
-            if (place_holder.id == correctPlacesForTwo[i]) {
-                place = correctPlacesForTwo[i]
-                return true
-            }
-        }
-    } else if (number_holder == "3") {
-        for (let i = 0; i < correctPlacesForThree.length; i++) {
-            if (place_holder.id == correctPlacesForThree[i]) {
-                place = correctPlacesForThree[i]
-                return true
-            }
-        }
-    } else if (number_holder == "4") {
-        for (let i = 0; i < correctPlacesForFour.length; i++) {
-            if (place_holder.id == correctPlacesForFour[i]) {
-                place = correctPlacesForFour[i]
-                return true
-            }
-        }
-    } else if (number_holder == "5") {
-        for (let i = 0; i < correctPlacesForFive.length; i++) {
-            if (place_holder.id == correctPlacesForFive[i]) {
-                place = correctPlacesForFive[i]
-                return true
-            }
-        }
-    } else if (number_holder == "6") {
-        for (let i = 0; i < correctPlacesForSix.length; i++) {
-            if (place_holder.id == correctPlacesForSix[i]) {
-                place = correctPlacesForSix[i]
-                return true
-            }
-        }
-    } else if (number_holder == "7") {
-        for (let i = 0; i < correctPlacesForSeven.length; i++) {
-            if (place_holder.id == correctPlacesForSeven[i]) {
-                place = correctPlacesForSeven[i]
-                return true
-            }
-        }
-    } else if (number_holder == "8") {
-        for (let i = 0; i < correctPlacesForEight.length; i++) {
-            if (place_holder.id == correctPlacesForEight[i]) {
-                place = correctPlacesForEight[i]
-                return true
-            }
-        }
-    } else if (number_holder == "9") {
-        for (let i = 0; i < correctPlacesForNine.length; i++) {
-            if (place_holder.id== correctPlacesForNine[i]) {
-                place = correctPlacesForNine[i]
-                return true
-            }
-        }
-    }
-    else {
-        return false
     }
 }
-function relocate() {
-    location.replace("")
+function fill_zeros() {
+    let zaros = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    ]
+    for (var i = 0; i < zaros.length; i++) {
+        id_out = i + ''
+        for (let j = 0; j < zaros[i].length; j++) {
+            id_in = id_out + j + ''
+            $(`#${id_in}`).text("");
+        }
+    }
+}
+function returnCheck(solved_board) {
+    let txt = place_holder + ''
+    let index = txt.split('')
+    if (solved_board[index[0]][index[1]] == number_holder) {
+        return true
+    } else {
+        return false
+    }
+
 }
 var timer = {
     Minutes: 0,
